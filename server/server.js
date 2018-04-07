927d901257417064d8aa1023fa57234baf847d74
emp-norton <jamesjelenko@gmail.com>
2018-04-07 16:09:48 -0700

    npm start script

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 let express = require('express');
 let app = express();
 let path = require('path');
+
 app.use(express.static(__dirname + '/../public'))
+
 app.get('/', (req, res) => {
 	 res.sendFile(path.join(__dirname + '/../public/autohorse.html'))
 })
 
-app.set('port', process.env.PORT || 3000);
 
+app.set('port', process.env.PORT || 3000);
 app.listen(port, function() {
 	console.log('listening on 3000');
 })
